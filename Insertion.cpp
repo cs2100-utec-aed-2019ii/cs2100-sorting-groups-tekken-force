62e24804ce4b10d9af6615ff6c40141fdb206259
johoyos <jonathan.hoyos@utec.edu.pe>
2019-08-28 10:36:24 -0500

    feat(insertion) : Add insertion sort
    created the inserion header .cpp and .h as derivated class from Sort

--- Insertion.cpp
+++ Insertion.cpp
@@ -3,7 +3,18 @@
     Insertion<T>::Insertion(T* arr, int size) : Sorting<T>(arr, size){}
 
     template<typename T>
-    void Insertion<T>::Sort_array()
-    {
+    void Insertion<T>::Sort_array(){
+	
+	size_t j;
+	for (size_t i =1 ; i< this->size ; i++){
+
+		j = i;
+		while (j>0 && this->arr[j-1]>this->arr[j]){
+			std::swap(this->arr[j], arr[j-1]);
+			j--;
+		}
+
+
+	}
 
     }
